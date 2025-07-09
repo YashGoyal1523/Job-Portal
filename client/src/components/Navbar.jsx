e432ae1a009e1cb8807972a7d8a431380925b2f5
YashGoyal1523 <yashgoyal1523@gmail.com>
2025-07-09 22:44:13 +0530

    Complete

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -19,15 +19,15 @@ const {setShowRecruiterLogin}=useContext(AppContext)
     <div className='shadow py-4'>
         <div className='container px-4 2xl:px-20 mx-auto flex justify-between items-center '>
 
-                    <div onClick={() => navigate('/')} className="flex items-center gap-2 cursor-pointer">
-                        <div className="w-10 h-10 bg-blue-600 rounded-full flex items-center justify-center">
-                          <FaUserTie className="text-white text-[14px]" />
-                        </div>
-                         <h1 className="text-[32px] font-bold tracking-wide">
-                         <span className="text-black">Job</span><span className="text-gray-600">ify</span>
-                         </h1>
-                    </div>
-
+                    <div onClick={() => navigate('/')} className="flex items-center gap-2 sm:gap-3 cursor-pointer">
+                          <div className="w-8 h-8 sm:w-10 sm:h-10 bg-blue-600 rounded-full flex items-center justify-center">
+                              <FaUserTie className="text-white text-[12px] sm:text-[14px]" />
+                          </div>
+                            <h1 className="text-xl sm:text-[34px] font-bold tracking-wide leading-none">
+                             <span className="text-black">Job</span>
+                             <span className="text-gray-600">ify</span>
+                            </h1>
+                          </div>
                      { user?
 
                    <div className='flex items-center gap-3'>
@@ -38,8 +38,8 @@ const {setShowRecruiterLogin}=useContext(AppContext)
                    </div>
                 :
             <div className='flex gap-4 max-sm:text-xs'>
-               <button onClick={() => setShowRecruiterLogin(true)}className='bg-gray-200 text-gray-700 px-6 sm:px-9 py-2 hover:bg-gray-300 rounded-full cursor-pointer'>Recruiter Login</button>
-                <button onClick={openSignIn} className='bg-blue-600 text-white px-6 sm:px-9 py-2 hover:bg-blue-700 rounded-full cursor-pointer'>User Login</button>
+               <button onClick={() => setShowRecruiterLogin(true)}className='bg-gray-200 text-gray-700 px-4 sm:px-6 py-1.5 sm:py-2 hover:bg-gray-300 rounded-full cursor-pointer'>Recruiter Login</button>
+                <button onClick={openSignIn} className='bg-blue-600 text-white px-4 sm:px-6 py-1.5 sm:py-2 hover:bg-blue-700 rounded-full cursor-pointer'>User Login</button>
             </div>
 
             }
